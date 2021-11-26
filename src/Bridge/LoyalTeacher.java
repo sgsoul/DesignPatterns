3b391daea0dac701d9fe286fd78570ad54d192a5
sgsoul <93263659+sgsoul@users.noreply.github.com>
2021-11-26 13:13:04 +0300

    Update LoyalTeacher.java

--- src/Bridge/LoyalTeacher.java
+++ src/Bridge/LoyalTeacher.java
@@ -2,7 +2,7 @@ package Bridge;
 
 public class LoyalTeacher implements Teacher{
     @Override
-    public void askQuestions() {
+    public void askQuestions(Lab lab) {
         System.out.println("Препод не задает ловушек джокера и не валит (зайка)");
     }
 }
