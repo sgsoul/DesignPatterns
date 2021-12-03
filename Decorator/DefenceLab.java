d09d95ba0a5221c076473f35d194b6752a0c9fbc
sgsoul <93263659+sgsoul@users.noreply.github.com>
2021-12-03 20:47:15 +0300

    Update DefenceLab.java

--- Decorator/DefenceLab.java
+++ Decorator/DefenceLab.java
@@ -2,7 +2,8 @@ package Decorator;
 
 public class DefenceLab {
     public static void main(String[] args) {
-        Student student = new SmartStudent(new DefaultStudent(new Student()));
+        IStudent student;
+        student = new SmartStudent(new DefaultStudent(new Student()));
 
         System.out.println(student.sdatLaby());
     }
